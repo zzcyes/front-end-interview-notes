d132a74661485ac266b3c9880d106a6e74ac42b0
zhongzichen <zhongzichen1997@163.com>
2022-05-28 16:30:25 +0800

    feat(code): :sparkles: 新增原型链调试代码

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.debug('debug main start~');
+
+
+import  { checkInstanceof } from './code/javascript/prototype-chain';
+
+console.debug('checkInstanceof：', checkInstanceof(1, Number));
+
+console.debug('debug main end~');
